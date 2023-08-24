7b2cd79f8703985375dc980828e857195d176a4e
Lucas Soares Tavares <130789583+tavaresslc@users.noreply.github.com>
2023-08-23 23:53:33 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
+// Importando as bibliotecas e estilos necessários
 import React, { useState } from "react";
 import "./styles.css";
 
+// Importando componentes personalizados
 import Navbar from "./components/Navbar/Navbar";
 import TaskList from "./components/TaskList/TaskList";
 
+// Variável para controlar a geração de IDs únicos para as tarefas
 let idAcc = 0;
+
+// Função para gerar um ID único para cada nova tarefa
 const generateId = () => {
   idAcc = idAcc + 1;
   return idAcc;
 };
 
+// Componente principal da aplicação
 export default function App() {
+  // Estado para armazenar a lista de tarefas
   const [tasks, setTasks] = useState([]);
 
+  // Função para adicionar uma nova tarefa à lista
   const addTask = (title, state) => {
     const newTask = {
       id: generateId(),
@@ -24,6 +32,7 @@ export default function App() {
     });
   };
 
+  // Função para atualizar os detalhes de uma tarefa existente
   const updateTask = (id, title, state) => {
     setTasks((existingTasks) => {
       return existingTasks.map((task) => {
@@ -36,16 +45,22 @@ export default function App() {
     });
   };
 
+  // Função para excluir uma tarefa da lista
   const deleteTask = (id) => {
-    setTasks((exitingTasks) => {
-      return exitingTasks.filter((task) => task.id !== id);
+    setTasks((existingTasks) => {
+      return existingTasks.filter((task) => task.id !== id);
     });
   };
 
+  // Renderização do componente principal
   return (
     <div className="App">
+      {/* Renderiza a barra de navegação */}
       <Navbar />
+
+      {/* Container para as listas de tarefas */}
       <div className="container">
+        {/* Lista de tarefas pendentes */}
         <TaskList
           title="Pendente"
           onAddTask={addTask}
@@ -54,6 +69,8 @@ export default function App() {
           onTaskUpdate={updateTask}
           onDeleteTask={deleteTask}
         />
+
+        {/* Lista de tarefas em progresso */}
         <TaskList
           title="Fazendo"
           onAddTask={addTask}
@@ -62,6 +79,8 @@ export default function App() {
           onTaskUpdate={updateTask}
           onDeleteTask={deleteTask}
         />
+
+        {/* Lista de tarefas completas */}
         <TaskList
           title="Completa"
           onAddTask={addTask}
